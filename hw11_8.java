9b4ff7c338e76b646fe2a21fb594d6cc38571a68
hanson97271 <92702155+hanson97271@users.noreply.github.com>
2022-01-05 00:08:02 +0800

    Add files via upload

---
+++ hw11_8.java
@@ -0,0 +1,29 @@
+package Java11;
+
+interface iAaa {
+	public void show();
+}
+class Cbbb{
+	public int num=10;
+	public void set(int n) {
+		num=n;
+	}
+}
+class Cccc extends Cbbb implement iAaa
+{
+	public void show(){
+		System.out.println("num"+num);
+	}
+}
+
+public class hw11_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cccc cc=new Cccc();
+		cc.set(5);
+		cc.show();
+
+	}
+
+}
